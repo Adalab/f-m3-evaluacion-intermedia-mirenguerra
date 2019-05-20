6e89bf60b23a93416487b94967d5825a717f46f3
mirenguerra <miren.guerra.sanz@gmail.com>
2019-05-20 11:46:34 +0200

    Create build folder

---
+++ docs/precache-manifest.c01397e355c20e3ada204b09c33e8f2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6695f9717268d4a597f3acb5aa25fd8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "058c701655c572f396e2",
+    "url": "/static/css/main.71c1ccc4.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "058c701655c572f396e2",
+    "url": "/static/js/main.61e9336b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
